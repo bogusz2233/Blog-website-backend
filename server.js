957c29e8551173ad573f710d838a2a918ee7491e
bogusz2233 <bogusz2233@gmail.com>
2019-01-19 12:25:58 +0100

    Added get function for /weather, now you can ask for currentyl weather

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // Żeby odpalić:  node server.js -e js,hbs lub nodemon server.js -e js,hbs
 const express = require('express');
 const homePage = require("./homePage/homePage");
+const weather = require("./weather/weather");
 
 const port = process.env.PORT || 3000;  // pobranie portu, gdy nie znajdzie wstawi 3000
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+//homePage get:
 app.get('/HomePageData',(req, res) => {
     res.set("Access-Control-Allow-Origin","*");
     res.send(homePage(req.query));
+});
+
+//weather get
+app.get("/weather", (request, response) => {
+    response.set("Access-Control-Allow-Origin","*");
+    console.log("Zapytanie get: " + request.query.get)
+    weather(request.query, response);
 });
 
 app.listen(port, () =>{ // port od heroku
---
+++ weather/weather.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+const IP_KEY_GEOCODE = "AIzaSyDgFQItK2bPF6UqPjZt6YBh-fc77OJJ82Y";
+const IP_KEY_DARKSKY = "ff392353fef24409879e40c8262de7a8";
+
+/*
+    Function return actual weather
+    argQuery - arguments passed in querry
+    responseCallback - reference to response object, use here to send back response
+*/
+var queryWheter = (argQuery, responseCallback) => {
+    var jsonResponse;   //response json
+    var encodedGeocodeArgs = encodeURIComponent(argQuery.place);
+    var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedGeocodeArgs}&key=${IP_KEY_GEOCODE}`;
+
+    // if place argument was't pass
+    if(argQuery.place ==undefined)
+    {
+        jsonResponse= {
+            errorMessage: "NO_PLACE_PARAM_PASSED"
+        };
+        responseCallback.send(jsonResponse);
+        return 0;
+    }
+    //send geocode request  
+    axios.get(geocodeUrl).then((response) => {
+        if (response.data.status === 'ZERO_RESULTS') {
+            throw new Error("Unable to find that address");
+        } else {
+            jsonResponse = {
+                lat: response.data.results[0].geometry.location.lat,
+                lng: response.data.results[0].geometry.location.lng,
+                formatted_address: response.data.results[0].formatted_address
+            }
+
+            //send darksky request  
+            var weatherUrl = `https://api.darksky.net/forecast/${IP_KEY_DARKSKY}/${jsonResponse.lat},${jsonResponse.lng}`;
+            return axios.get(weatherUrl);
+        }
+
+    }).then((weatherResponse) => {
+        if(argQuery.get == "currently")
+        {
+            jsonResponse.currently = weatherResponse.data.currently;
+        }
+        else
+        {
+            jsonResponse.errorMessage = "NO_GET_PARAM_PASSED";
+        }
+        responseCallback.send(jsonResponse);
+    }).catch((err) => {
+        if (err.code === 'ENOTFOUND') {
+            jsonResponse.errorMessage = "UNABLE_TO_CONNECT_URL";
+            
+        } else {
+            jsonResponse.errorMessage = err.message;
+        }
+        responseCallback.send(jsonResponse);
+    });
+};
+
+module.exports = queryWheter;
